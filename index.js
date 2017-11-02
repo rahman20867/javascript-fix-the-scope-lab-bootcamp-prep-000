d8763df6a2a515b224b6058b388752a61bf5be88
Marvin Andrade <marvin@lionguardsecurity.com>
2017-11-02 01:12:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // : you only need to modify the code below this line.
 
-var theFunk = funkyFunction()
+var theFunk = return funkyFunction()
 
-return theFunk
+
